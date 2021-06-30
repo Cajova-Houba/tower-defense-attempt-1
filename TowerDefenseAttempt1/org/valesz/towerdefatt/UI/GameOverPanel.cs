5e5ca33df7e19e3d75f91aec9884ab99a5f2a765
Zdenek Vales <zdenek.vales@yoso.fi>
2021-06-30 20:25:23 +0200

    NaN: Unused dependencies removed.

--- TowerDefenseAttempt1/org/valesz/towerdefatt/UI/GameOverPanel.cs
+++ TowerDefenseAttempt1/org/valesz/towerdefatt/UI/GameOverPanel.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Xna.Framework.Graphics
 using TowerDefenseAttempt1.org.valesz.towerdefatt.Core;
 using TowerDefenseAttempt1.org.valesz.towerdefatt.UI.Buttons;
 
